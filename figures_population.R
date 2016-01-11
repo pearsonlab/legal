c9631bc184d8b3c83aded7a45b2316aaf1eca8cf
John Pearson <jmxpearson@gmail.com>
2016-01-11 11:13:54 -0500

    Figure 2A working

--- figures_population.R
+++ figures_population.R
@@ -120,25 +120,25 @@ plt <- ggplot() +
     legend.position=c(1,1),
     legend.justification=c(1,1))
 
-
 ################### FIGURE 2A - EVIDENCE EFFECTS ON CONFIDENCE IN GUILT
 
-#fe <- fixed_effects[fixed_effects$predictor %in% ev_vars,]
-fe<-dfevconf
-fe$factor <- factor(fe$factor, levels=c('physical2', 'physical1','witness1','history2','history1'))
+fe <- fixed_effects %>% filter(outcome == 'rating', predictor.2 %in% ev_vars) %>%
+  rename(group=predictor.1, predictor=predictor.2) %>%
+  mutate(predictor=factor(predictor, levels=c('physical2', 'physical1','witness1','history2','history1')))
 
 plt_ev <- ggplot(data = fe) +
-  geom_pointrange(aes(x=factor, y=post.mean, ymin=l95.CI, ymax=u95.CI, color=predictor), size=1.75, position=position_jitter(w=0.15)) + 
+  geom_pointrange(aes(x=predictor, y=post.mean, ymin=l95.CI, ymax=u95.CI, 
+                      color=group), size=1.75, position=position_jitter(w=0.15)) + 
   scale_x_discrete(breaks=c("history1","history2","physical1","physical2","witness1"), 
-                   labels=c("Unrelated \nprior crime", "Related \nprior crime", "Non-DNA \nphysical \nevidence", "DNA \nphysical \nevidence", "Witness \npresent"))+
-  coord_cartesian(ylim=c(0,50))+
-  labs(title="A")+
-  ylab("Effect size (points above baseline)")+
-  xlab("")+
-  geom_vline(xintercept=1.5, colour='grey')+
-  geom_vline(xintercept=2.5, colour='grey')+
-  geom_vline(xintercept=3.5, colour='grey')+
-  geom_vline(xintercept=4.5, colour='grey')+
+                   labels=c("Unrelated \nprior crime", "Related \nprior crime", "Non-DNA \nphysical \nevidence", "DNA \nphysical \nevidence", "Witness \npresent")) +
+  coord_cartesian(ylim=c(0,50)) +
+  labs(title="A") +
+  ylab("Effect size (points above baseline)") +
+  xlab("") +
+  geom_vline(xintercept=1.5, colour='grey') +
+  geom_vline(xintercept=2.5, colour='grey') +
+  geom_vline(xintercept=3.5, colour='grey') +
+  geom_vline(xintercept=4.5, colour='grey') +
   theme(
     panel.grid=element_blank(),
     panel.background = element_blank(),
