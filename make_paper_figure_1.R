97e72fab36873dc1d44fc22f4c3d0f5ff3170ef9
John Pearson <jmxpearson@gmail.com>
2017-08-28 09:05:25 -0400

    changes to fig 1
    - reordering panels c, d
    - adding fancy x axis label to F1B

--- make_paper_figure_1.R
+++ make_paper_figure_1.R
@@ -34,6 +34,11 @@ ev_codes <- sc_means %>% select(physical, witness, history) %>%
 
 # add code column to scenario means
 sc_means <- sc_means %>% merge(ev_codes) %>% arrange(code, scenario)
+code_means <- sc_means %>% group_by(physical, witness, history) %>% 
+  summarise(rating=mean(rating)) %>%
+  ungroup() %>%
+  mutate_at(c("history"), funs(substr(as.character(.), 1, 1))) %>%
+  mutate_at(c("witness"), funs(sapply(strsplit(as.character(.), " "), `[`, 1)))
 
 # calculate model prediction
 # get mean values of coefficients across scenarios
@@ -49,18 +54,35 @@ colnames(Xmat)[1] <- 'baseline'
 Xmat <- Xmat[, sort(colnames(Xmat), index.return=TRUE)$ix]
 preds <- data.frame(code=ev_codes$code, pred=Xmat %*% betas$effect)
 
-plt_2 <- ggplot(sc_means) +
+plt_2a <- ggplot(sc_means) +
      geom_boxplot(aes(code, rating), outlier.size=0, outlier.stroke=0) +
      geom_jitter(aes(code, rating), width=0.2, alpha=0.5, color=color_genpop) +
      geom_point(data=preds, aes(x=code, y=pred), color="red", shape=5, size=5, stroke=2) +
-     xlab('Evidence Combinations') + ylab('Mean case strength (points)') +
-     ylim(0, 100) +
+     ylab('Mean case strength (points)') +
+     coord_cartesian(ylim = c(-0, 100), expand = TRUE) +
      labs(title="B", size=rel(3)) +
+     annotate(geom = "text", x = seq_len(18), y = -8, 
+              label = code_means$history, size = 4) +
+     annotate(geom = "text", x = -0, 
+              y = -8, label = "Prior conviction", size = 4, hjust=1) +
+     annotate(geom = "text", x = seq(2, 18, 3), 
+              y = -15, label = code_means$witness[seq(2, 18, 3)], size = 4) +
+     annotate(geom = "text", x = -0, 
+              y = -15, label = "Witness", size = 4, hjust=1) +
+     annotate(geom = "text", x = seq(3.5, 18, 6), 
+              y = -22, label = code_means$physical[seq(3, 18, 6)], size = 4) +
+     annotate(geom = "text", x = -0, 
+              y = -22, label = "Physical evidence", size = 4, hjust=1) +
      th +
      theme(
-       axis.text.x = element_blank()
+       plot.margin=unit(c(25.5, 25.5, 50.5, 25.5), "points"),
+       axis.text.x = element_blank(),
+       axis.title.x = element_blank()
      )
 
+plt_2 <- ggplot_gtable(ggplot_build(plt_2a))
+plt_2$layout$clip[plt_2$layout$name == "panel"] <- "off"
+grid::grid.draw(plt_2)
 ####################
 
 # plot punishment ratings for each scenario
@@ -93,7 +115,7 @@ plt_3 <- ggplot(df) +
                    labels=seriousness$scenario) + 
   coord_cartesian(ylim=c(0, 100)) +
   ylab("Punishment Rating (points)") +
-  labs(title="C", size=rel(3)) +
+  labs(title="D", size=rel(3)) +
   th +
   theme(
     axis.text.x = element_text(size=rel(0.75))
@@ -120,7 +142,7 @@ plt_4 <- ggplot(pred_dat) + geom_line(aes(x=rating, y=prob)) +
   stat_summary_bin(data=dat, aes(x=rating, y=guilty), fun.data = "mean_cl_boot") +
   xlab("Case Strength (points)") +
   ylab("Probability of Voting Guilty") +
-  labs(title="D", size=rel(3)) +
+  labs(title="C", size=rel(3)) +
   th
 
 #Scenario make a list of panels
@@ -134,7 +156,7 @@ max_heights <- do.call(unit.pmax, lapply(plt_list, function(x) {x$heights}))
 grob_list <- lapply(plt_list, function(x) {x$heights <- max_heights; x})
 
 # arrange with differing widths
-lay <- rbind(c(1, 2), c(3, 4))
+lay <- rbind(c(1, 2), c(4, 3))
 plt_all <- do.call(arrangeGrob, c(plt_list, ncol=2, layout_matrix=list(lay),
                                   widths=list(c(1, 1))))
 # plt_all <- arrangeGrob(plt_1, plt_2, plt_3)
